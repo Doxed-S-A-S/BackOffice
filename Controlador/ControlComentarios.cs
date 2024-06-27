b38c23beae37350c3ad45cc543a03b46b66c425e
MatiasFGarciaDox <mfernandez8@estudiante.ceibal.edu.uy>
2024-06-27 14:35:40 -0300

    Modelo y parte del control de comentarios

---
+++ Controlador/ControlComentarios.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using Modelos;
+
+namespace Controlador
+{
+    public class ControlComentarios
+    {
+        public static void CrearComentario(int idPost,string comentario,string reacciones)
+        {
+            ModeloComentario coment = new ModeloComentario();
+            coment.IdPost = idPost;
+            coment.Comentario = comentario;
+            coment.ReaccionesCom = Int32.Parse(reacciones);
+
+            coment.GuardarComentario();
+        }
+
+        public static void EliminarComentario(string idcoment)
+        {
+            ModeloComentario coment = new ModeloComentario();
+            coment.IdComentario = Int32.Parse(idcoment);
+            coment.GuardarComentario();
+        }
+
+
+    }
+}
--- Modelo/ModeloComentario.cs
+++ Modelo/ModeloComentario.cs
@@ -4,9 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Modelo
+namespace Modelos
 {
-    class ModeloComentario
+    public class ModeloComentario : Modelo
     {
+        public int IdPost;
+        public int IdComentario;
+        public string Comentario;
+        public int ReaccionesCom;
+
+        public void GuardarComentario()
+        {
+            string sql = $"insert into Comentarios (Comentario,C_Reacciones) values('{this.Comentario}',{this.ReaccionesCom})";
+            this.Comando.CommandText = sql;
+            this.Comando.ExecuteNonQuery();
+        }
+
+        public void EliminarComentario()
+        {
+            string sql = $"update Comentarios set Eliminado = true where ID_comentario ='{this.IdComentario}'";
+            this.Comando.CommandText = sql;
+            this.Comando.ExecuteNonQuery();
+        }
+
+        public List<ModeloComentario> ObtenerComentarios()
+        {
+            List<ModeloComentario> comentarios = new List<ModeloComentario>();
+
+            string sql = $"select * from Comentarios where ID_post = '{this.IdPost}' and Eliminado = false";
+            this.Comando.CommandText = sql;
+            this.Comando.ExecuteNonQuery();
+
+            while (this.Lector.Read())
+            {
+                ModeloComentario coment = new ModeloComentario();
+                coment.IdPost = Int32.Parse(this.Lector["IdPost"].ToString());
+                coment.IdComentario = Int32.Parse(this.Lector["IdComentario"].ToString());
+                coment.Comentario = this.Lector["Comentario"].ToString();
+                coment.ReaccionesCom = Int32.Parse(this.Lector["ReaccionesCom"].ToString());
+                comentarios.Add(coment);
+            }
+            return comentarios;
+        }
     }
 }
