8f5b0657192ba4ba96159cb82952c6b2c981d9ac
MatiasFGarciaDox <mfernandez8@estudiante.ceibal.edu.uy>
2024-11-08 21:21:44 -0300

    Small fixes graficos

--- Interfaz/AdministracionPost.Designer.cs
+++ Interfaz/AdministracionPost.Designer.cs
@@ -42,6 +42,11 @@ namespace Interfaz
             this.LbIdPost = new System.Windows.Forms.Label();
             this.LbNumeroDeReports = new System.Windows.Forms.Label();
             this.LbFechaDePost = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.DgridComentarios)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,7 +54,7 @@ namespace Interfaz
             // 
             this.BtnEliminarPost.BackColor = System.Drawing.Color.Red;
             this.BtnEliminarPost.ForeColor = System.Drawing.Color.White;
-            this.BtnEliminarPost.Location = new System.Drawing.Point(106, 159);
+            this.BtnEliminarPost.Location = new System.Drawing.Point(176, 148);
             this.BtnEliminarPost.Name = "BtnEliminarPost";
             this.BtnEliminarPost.Size = new System.Drawing.Size(75, 23);
             this.BtnEliminarPost.TabIndex = 34;
@@ -58,19 +63,19 @@ namespace Interfaz
             // 
             // BtnEditarPost
             // 
-            this.BtnEditarPost.Location = new System.Drawing.Point(12, 159);
+            this.BtnEditarPost.Location = new System.Drawing.Point(12, 148);
             this.BtnEditarPost.Name = "BtnEditarPost";
             this.BtnEditarPost.Size = new System.Drawing.Size(75, 23);
             this.BtnEditarPost.TabIndex = 33;
-            this.BtnEditarPost.Text = "Editar";
+            this.BtnEditarPost.Text = "Modificar";
             this.BtnEditarPost.UseVisualStyleBackColor = true;
             // 
             // TboxPublicacion
             // 
-            this.TboxPublicacion.Location = new System.Drawing.Point(12, 41);
+            this.TboxPublicacion.Location = new System.Drawing.Point(12, 54);
             this.TboxPublicacion.Multiline = true;
             this.TboxPublicacion.Name = "TboxPublicacion";
-            this.TboxPublicacion.Size = new System.Drawing.Size(169, 112);
+            this.TboxPublicacion.Size = new System.Drawing.Size(239, 88);
             this.TboxPublicacion.TabIndex = 32;
             // 
             // DgridComentarios
@@ -81,16 +86,16 @@ namespace Interfaz
             this.DgridComentarios.AllowUserToResizeRows = false;
             this.DgridComentarios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.DgridComentarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DgridComentarios.Location = new System.Drawing.Point(12, 216);
+            this.DgridComentarios.Location = new System.Drawing.Point(12, 281);
             this.DgridComentarios.Name = "DgridComentarios";
-            this.DgridComentarios.Size = new System.Drawing.Size(169, 129);
+            this.DgridComentarios.Size = new System.Drawing.Size(239, 85);
             this.DgridComentarios.TabIndex = 37;
             // 
             // BtnEliminarComentario
             // 
             this.BtnEliminarComentario.BackColor = System.Drawing.Color.Red;
             this.BtnEliminarComentario.ForeColor = System.Drawing.Color.White;
-            this.BtnEliminarComentario.Location = new System.Drawing.Point(106, 441);
+            this.BtnEliminarComentario.Location = new System.Drawing.Point(176, 466);
             this.BtnEliminarComentario.Name = "BtnEliminarComentario";
             this.BtnEliminarComentario.Size = new System.Drawing.Size(75, 23);
             this.BtnEliminarComentario.TabIndex = 43;
@@ -99,24 +104,24 @@ namespace Interfaz
             // 
             // BtnEditarComentario
             // 
-            this.BtnEditarComentario.Location = new System.Drawing.Point(12, 441);
+            this.BtnEditarComentario.Location = new System.Drawing.Point(12, 466);
             this.BtnEditarComentario.Name = "BtnEditarComentario";
             this.BtnEditarComentario.Size = new System.Drawing.Size(75, 23);
             this.BtnEditarComentario.TabIndex = 42;
-            this.BtnEditarComentario.Text = "Editar";
+            this.BtnEditarComentario.Text = "Modificar";
             this.BtnEditarComentario.UseVisualStyleBackColor = true;
             // 
             // TboxComentarios
             // 
-            this.TboxComentarios.Location = new System.Drawing.Point(12, 351);
+            this.TboxComentarios.Location = new System.Drawing.Point(12, 372);
             this.TboxComentarios.Multiline = true;
             this.TboxComentarios.Name = "TboxComentarios";
-            this.TboxComentarios.Size = new System.Drawing.Size(169, 84);
+            this.TboxComentarios.Size = new System.Drawing.Size(240, 88);
             this.TboxComentarios.TabIndex = 41;
             // 
             // BtnPublicacionResuelta
             // 
-            this.BtnPublicacionResuelta.Location = new System.Drawing.Point(106, 483);
+            this.BtnPublicacionResuelta.Location = new System.Drawing.Point(176, 513);
             this.BtnPublicacionResuelta.Name = "BtnPublicacionResuelta";
             this.BtnPublicacionResuelta.Size = new System.Drawing.Size(75, 26);
             this.BtnPublicacionResuelta.TabIndex = 44;
@@ -125,17 +130,17 @@ namespace Interfaz
             // 
             // BtnMostrarMultimedia
             // 
-            this.BtnMostrarMultimedia.Location = new System.Drawing.Point(12, 188);
+            this.BtnMostrarMultimedia.Location = new System.Drawing.Point(175, 202);
             this.BtnMostrarMultimedia.Name = "BtnMostrarMultimedia";
             this.BtnMostrarMultimedia.Size = new System.Drawing.Size(75, 23);
             this.BtnMostrarMultimedia.TabIndex = 45;
-            this.BtnMostrarMultimedia.Text = "Multimedia";
+            this.BtnMostrarMultimedia.Text = "Modificar";
             this.BtnMostrarMultimedia.UseVisualStyleBackColor = true;
             // 
             // LbUserId
             // 
             this.LbUserId.AutoSize = true;
-            this.LbUserId.Location = new System.Drawing.Point(12, 9);
+            this.LbUserId.Location = new System.Drawing.Point(12, 5);
             this.LbUserId.Name = "LbUserId";
             this.LbUserId.Size = new System.Drawing.Size(46, 13);
             this.LbUserId.TabIndex = 46;
@@ -144,7 +149,7 @@ namespace Interfaz
             // LbIdPost
             // 
             this.LbIdPost.AutoSize = true;
-            this.LbIdPost.Location = new System.Drawing.Point(139, 9);
+            this.LbIdPost.Location = new System.Drawing.Point(139, 5);
             this.LbIdPost.Name = "LbIdPost";
             this.LbIdPost.Size = new System.Drawing.Size(42, 13);
             this.LbIdPost.TabIndex = 47;
@@ -153,7 +158,7 @@ namespace Interfaz
             // LbNumeroDeReports
             // 
             this.LbNumeroDeReports.AutoSize = true;
-            this.LbNumeroDeReports.Location = new System.Drawing.Point(12, 490);
+            this.LbNumeroDeReports.Location = new System.Drawing.Point(12, 520);
             this.LbNumeroDeReports.Name = "LbNumeroDeReports";
             this.LbNumeroDeReports.Size = new System.Drawing.Size(72, 13);
             this.LbNumeroDeReports.TabIndex = 48;
@@ -162,17 +167,65 @@ namespace Interfaz
             // LbFechaDePost
             // 
             this.LbFechaDePost.AutoSize = true;
-            this.LbFechaDePost.Location = new System.Drawing.Point(12, 25);
+            this.LbFechaDePost.Location = new System.Drawing.Point(12, 22);
             this.LbFechaDePost.Name = "LbFechaDePost";
             this.LbFechaDePost.Size = new System.Drawing.Size(99, 13);
             this.LbFechaDePost.TabIndex = 49;
             this.LbFechaDePost.Text = "Fecha de creacion:";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 204);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 20);
+            this.textBox1.TabIndex = 50;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 51;
+            this.label1.Text = "Contenido del post:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 52;
+            this.label2.Text = "Contenido multimedia";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 53;
+            this.label3.Text = "Comentarios del post:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 54;
+            this.button1.Text = "Ver imagen del post";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // AdministracionPost
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(193, 521);
+            this.ClientSize = new System.Drawing.Size(263, 542);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.LbFechaDePost);
             this.Controls.Add(this.LbNumeroDeReports);
             this.Controls.Add(this.LbIdPost);
@@ -211,5 +264,10 @@ namespace Interfaz
         private System.Windows.Forms.Label LbIdPost;
         private System.Windows.Forms.Label LbNumeroDeReports;
         private System.Windows.Forms.Label LbFechaDePost;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
